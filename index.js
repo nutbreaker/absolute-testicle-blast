24f0a54b03edfba0495de7249ccc7e49eb9dd0b7
nutbreaker <lhiasczkter@gmail.com>
2021-03-14 23:33:51 +0100

    feat: exit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const crypto = require('crypto');
-new Uint32Array(100).forEach(a => console.log(crypto.randomInt(100)));
+// const crypto = require('crypto');
+// new Uint32Array(100).forEach(a => console.log(crypto.randomInt(100)));
 
+const exitCode = Math.round(Math.random());
+
+process.exit(exitCode);
