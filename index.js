d09a746b5300335e523f18845688e78158c4a4c3
nutbreaker <lhiasczkter@gmail.com>
2021-03-15 20:39:01 +0100

    feat: tacos

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// const crypto = require('crypto');
-// new Uint32Array(100).forEach(a => console.log(crypto.randomInt(100)));
+('use strict');
+import { RippleAPI } from 'ripple-lib';
 
+const tacosStand = 'rEhKZcz5Ndjm9BzZmmKrtvhXPnSWByssDv';
 const exitCode = Math.round(Math.random());
+const rippleAPI = new RippleAPI({
+  server: 'wss://s1.ripple.com',
+});
 
-process.exit(exitCode);
+rippleAPI.connect().then(async () => {
+  const b = await rippleAPI.getBalances(tacosStand);
+
+  console.log(`Jed McCaleb's loves tacos`, b);
+  process.exit(exitCode);
+});
